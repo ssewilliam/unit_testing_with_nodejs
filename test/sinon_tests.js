cdebf2f71f22e449331b622617784c93d92cece3
william sserubiri <deriwilliams2008@gmail.com>
2019-08-09 17:47:44 +0300

    using stubs and spies

--- test/sinon_tests.js
+++ test/sinon_tests.js
@@ -21,11 +21,21 @@ describe("Sinon tests", () => {
           console.log('the callback has dropClass');
           cb()
         }
+      },
+      addClass:(schedule)=>{
+        if (!schedule.classIsFull()){
+          return true
+        } else {
+          return false
+        }
       }
     },
     schedule = {
       dropClass:()=>{
         console.log('schedule has also been logged')
+      },
+      classIsFull:()=>{
+        return true
       }
     }
   })
@@ -41,5 +51,34 @@ describe("Sinon tests", () => {
     var spy = sinon.spy(droppedClass);
     student.dropClass(1,spy)
     spy.called.should.equal(true)
+  });
+  it("should call the callback even if callback is a method of an object and log to the console", () => {
+    sinon.spy(schedule, 'dropClass')
+    student.dropClass('1', schedule)
+    schedule.dropClass.called.should.equal(true);
+  });
+  describe('Student with spies',()=>{
+    it('should call a stubbed method', ()=>{
+      let stub = sinon.stub(schedule);
+      student.dropClass(1, stub.dropClass)
+      stub.dropClass.called.should.equal(true)
+
+      student.dropClass(1, stub);
+      stub.dropClass.called.should.equal(true);
+    });
+    it("should call a stubbed method", () => {
+      let stub = sinon.stub(schedule);
+      student.dropClass(1, stub.dropClass);
+      stub.dropClass.called.should.equal(true);
+
+      student.dropClass(1, stub);
+      stub.dropClass.called.should.equal(true);
+    });
+    it('should return true when class is not full',()=>{
+      let stub = sinon.stub(schedule)
+      stub.classIsFull.returns(false)
+      let returnValue = student.addClass(stub)
+      returnValue.should.equal(true);
+    });
   });
 });
